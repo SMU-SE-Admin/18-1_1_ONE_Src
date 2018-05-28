58b411323e72d4440bd2b5cd6952f0ae81fc092f
IAN <IAN@DESKTOP-8JNHFRS>
2018-05-28 17:18:25 +0900

    #1153 #1151 #1155

---
+++ login.java
@@ -0,0 +1,186 @@
+package jikong;
+
+import java.util.*;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.event.*;
+
+
+public class login extends JFrame{
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField2;
+	
+	public login(HashMap<String,Account> map)
+	{
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(200,200,400,400);
+		JPanel contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(null);
+		setContentPane(contentPane);
+		setTitle("jikong");
+		//contentPane.setBackground(Color.WHITE);
+	
+	JLabel Label = new JLabel();
+	Label.setText("                	   로그인");
+	Label.setOpaque(true);
+	Label.setFont(new Font("돋음",Font.PLAIN,30));
+	Label.setBounds(0,0,400,75);
+	contentPane.add(Label);
+
+	JLabel Label2 = new JLabel();
+	Label2.setText("아이디");
+	Label2.setOpaque(true);
+	Label2.setFont(new Font("돋음",Font.PLAIN,20));
+	Label2.setBounds(50,80,60,40);
+	contentPane.add(Label2);
+	
+	JLabel Label3 = new JLabel();
+	Label3.setText("비밀번호");
+	Label3.setOpaque(true);
+	Label3.setFont(new Font("돋음",Font.PLAIN,20));
+	Label3.setBounds(50,130,100,40);
+	contentPane.add(Label3);
+	
+	textField = new JTextField();
+	textField.setBounds(150,85,180,30);
+	contentPane.add(textField);
+
+	textField2 = new JTextField();
+	textField2.setBounds(150,135,180,30);
+	contentPane.add(textField2);
+
+	JButton Button = new JButton("ID찾기");
+	Button.setBackground(Color.GREEN);
+	Button.setForeground(Color.BLACK);
+	Button.addActionListener(new ActionListener()
+			{
+		public void actionPerformed(ActionEvent arg0)
+		{
+			EventQueue.invokeLater(new Runnable()
+					{
+				public void run()
+				{
+					try
+					{
+						setVisible(false);
+						searchID frame = new searchID(map);
+						frame.setVisible(true);
+					}catch (Exception e)
+					{
+						e.printStackTrace();
+					}
+				}
+					});
+		}
+			});
+	Button.setBounds(60,215,115,40);
+	contentPane.add(Button);
+
+	JButton Button2 = new JButton("PW 찾기");
+	Button2.setBackground(Color.GREEN);
+	Button2.setForeground(Color.BLACK);
+	Button2.addActionListener(new ActionListener()
+			{
+		public void actionPerformed(ActionEvent arg0)
+		{
+			EventQueue.invokeLater(new Runnable()
+					{
+				public void run()
+				{
+					try
+					{
+						setVisible(false);
+						searchpw frame = new searchpw(map);
+						frame.setVisible(true);
+					}catch (Exception e)
+					{
+						e.printStackTrace();
+					}
+				}
+					});
+		}
+			});
+	Button2.setBounds(60,260,115,40);
+	contentPane.add(Button2);
+
+	JButton Button3 = new JButton("회원가입");
+	Button3.setBackground(Color.GREEN);
+	Button3.setForeground(Color.BLACK);
+	Button3.addActionListener(new ActionListener()
+			{
+		public void actionPerformed(ActionEvent arg0)
+		{
+			EventQueue.invokeLater(new Runnable()
+					{
+				public void run()
+				{
+					try
+					{
+						setVisible(false);
+						signup frame = new signup(map);
+						frame.setVisible(true);
+
+						
+					}catch (Exception e)
+					{
+						e.printStackTrace();
+					}
+				}
+					});
+		}
+			});
+	Button3.setBounds(195,215,115,40);
+	contentPane.add(Button3);
+
+	JButton Button4 = new JButton("로그인");
+	Button4.setBackground(Color.GREEN);
+	Button4.setForeground(Color.BLACK);
+	Button4.addActionListener(new ActionListener()
+			{
+		public void actionPerformed(ActionEvent arg0)
+		{
+			EventQueue.invokeLater(new Runnable()
+					{
+				public void run()
+				{
+					try
+					{
+						String ID = textField.getText();
+						String PW = textField2.getText();
+						Set<String> ids = map.keySet();
+						Iterator<String> it = ids.iterator();
+						int num=0;
+						
+						while(it.hasNext()) {
+							String dicid = it.next();
+							Account dicac = map.get(dicid);
+							if(ID.equals(dicid)&&PW.equals(dicac.getpasswd())) {
+								setVisible(false);
+								main frame = new main();
+								frame.setVisible(true);
+								num++;
+							}
+						}
+						
+						if(num==0) {
+							textField.setText("");
+							textField2.setText("");
+						}
+						
+					}catch (Exception e)
+					{
+						e.printStackTrace();
+					}
+				}
+					});
+		}
+			});
+	Button4.setBounds(195,260,115,40);
+	contentPane.add(Button4);
+	}
+}
---
+++ searchpw.java
@@ -0,0 +1,75 @@
+package jikong;
+
+import java.util.*;
+
+import java.awt.event.*;
+import java.awt.*;
+
+import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.table.DefaultTableModel;
+
+public class searchpw extends JFrame{
+
+	private JPanel contentPane;
+	private JTextField textField;
+
+	public searchpw(HashMap<String,Account> map)
+	{
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(200, 200, 400, 150);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBackground(Color.GREEN);
+		contentPane.setLayout(null);
+		setContentPane(contentPane);
+		//contentPane.setBackground(Color.WHITE);
+		setTitle("               아이디찾기");
+		
+		JLabel Label = new JLabel();
+		
+		Label.setText("아이디계정입력: ");
+		Label.setOpaque(true);
+		Label.setBackground(Color.GREEN);
+		//NewLabel.setBackground(Color.WHITE);
+		Label.setFont(new Font("돋음", Font.PLAIN, 15));
+		Label.setBounds(0, 20, 130, 30);
+		contentPane.add(Label);
+
+		textField = new JTextField();
+		textField.setBounds(130,20,200,30);
+		contentPane.add(textField);
+
+		JButton Button = new JButton("확인");
+		Button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							Set<String> ids = map.keySet();
+							Iterator<String> it = ids.iterator();
+							String putID = textField.getText();
+							if(!map.containsKey(putID)) {
+								searchpwok frame = new searchpwok(map,putID,0);
+								frame.setVisible(true);
+								setVisible(false);
+								}
+							else {
+								searchpwok frame = new searchpwok(map,putID,1);
+								frame.setVisible(true);
+								setVisible(false);
+								}
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+		});
+		Button.setBounds(135, 60, 100, 40);
+		Button.setBackground(Color.YELLOW);
+		Button.setForeground(Color.BLACK);
+		Button.setFont(new Font("돋음", Font.PLAIN, 15));
+		contentPane.add(Button);
+	}
+}
