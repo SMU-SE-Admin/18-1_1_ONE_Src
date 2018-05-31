daa6c89996a91714c9816bc4a22fe7d05ace99a0
201611037 <37533690+201611037@users.noreply.github.com>
2018-05-31 21:17:00 +0900

    #1148, #1151, #1153, #1155 코드 수정과 일감 번호 수정
    코드 수정과 일감 번호 수정

--- src/main/java/se/smu/start.java
+++ src/main/java/se/smu/start.java
@@ -1,4 +1,4 @@
-package jikong;
+package se.smu;
 
 import java.util.*;
 
@@ -21,8 +21,36 @@ class Account {
 	String getnickname() {return nickname;}
 }
 
-public class start {
+class List {
+	ArrayList<String> strtodo = new ArrayList<String>(4);
+	ArrayList<Integer> inttodo= new ArrayList<Integer>(5);
+	private String sub; private String pro;
+	int time; int grade; int sem; String todo;
+	int deadline; int realdead; String ox;
+	
+	public List(String sub,String pro,int time,int grade,int sem) {
+		strtodo.add(0,sub); strtodo.add(1,pro);
+		inttodo.add(0,time); inttodo.add(1,grade); inttodo.add(2,sem);
+	//this.sub=sub; this.pro=pro; this.time=time; this.grade=grade; this.sem=sem;
+	}
+	public List(String todo,int deadline,int realdead,String ox) {
+		strtodo.add(2,todo); inttodo.add(3,deadline);
+		inttodo.add(4,realdead); strtodo.add(3,ox);
+	//this.todo=todo; this.deadline=deadline; this.realdead=realdead; this.ox=ox;
+	}
+	
+	String getsub() {return strtodo.get(0);}
+	String getpro() {return strtodo.get(1);}
+	int gettime() {return inttodo.get(0);}
+	int getgrade() {return inttodo.get(1);}
+	int getsem() {return inttodo.get(2);}
+	String gettodo() {return strtodo.get(2);}
+	int getdeadline() {return inttodo.get(3);}
+	int getrealdead() {return inttodo.get(4);}
+	String getox() {return strtodo.get(3);}
+}
 
+public class start {
 	public static void main(String[] args)
 	{
 		EventQueue.invokeLater(new Runnable()
@@ -44,3 +72,5 @@ public class start {
 	}	
 	
 }
+
+
